3ae472729309ae5ba0948283147d09dd6eb986eb
Sasanksarma <32011567+Sasanksarma@users.noreply.github.com>
2023-04-26 16:15:00 +0530

    changed the port number from 5001 to 5010

--- square/square.py
+++ square/square.py
@@ -20,4 +20,4 @@ def square():
     return f'The {operation} of {number} is {square}.'
 
 if __name__ == '__main__':
-    app.run(port=5001,debug = True,host = '0.0.0.0')
+    app.run(port=5010,debug = True,host = '0.0.0.0')
