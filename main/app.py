f099c8c4bf1ced685ac32b8f314a80b62fce506d
Sasanksarma <32011567+Sasanksarma@users.noreply.github.com>
2023-04-26 16:13:43 +0530

    changed the port number from 8000 to 8021

--- main/app.py
+++ main/app.py
@@ -44,4 +44,4 @@ def store_metric(table_name, metric):
     conn.close()
 
 if __name__ == '__main__':
-    app.run(port=8000,host='0.0.0.0',debug= True)
+    app.run(port=8021,host='0.0.0.0',debug= True)
