14c551100ed7aa3f16a7564ce3e9693eb830bdfe
Sasanksarma <32011567+Sasanksarma@users.noreply.github.com>
2023-04-26 15:56:02 +0530

    true to True

--- main/app.py
+++ main/app.py
@@ -44,4 +44,4 @@ def store_metric(table_name, metric):
     conn.close()
 
 if __name__ == '__main__':
-    app.run(port=8000,host='0.0.0.0',debug=true)
+    app.run(port=8000,host='0.0.0.0',debug= True)
